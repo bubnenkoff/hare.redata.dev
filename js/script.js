0e10da6c3165237d7abfbdced16afdaa40c52c66
Dmitry <bubnenkoff@gmail.com>
2024-11-06 16:20:39 +0300

    remove all

--- js/script.js
+++
@@ -1,6 +0,0 @@
-const eventSource = new EventSource('/events');
-
-eventSource.onmessage = function(event) {
-  console.log('Новое событие:', event.data);
-  location.reload(); // Или обновление части страницы.
-};
